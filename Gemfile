49862af72496361ae1a6cb0b286989531e337df3
razan123 <rajan4ualwz@github.com>
2015-01-07 16:26:34 +0545

    Features added

--- Gemfile
+++ Gemfile
@@ -6,6 +6,9 @@ gem 'rails', '3.2.21'
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
 gem 'mysql2'
+group :development do
+	gem 'pry'
+end
 
 
 # Gems used only for assets and not required
--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,40 @@
 class ProjectsController < ApplicationController
+
 	def new
-		@project=Project.new
-	end	
+		@project = Project.new
+	end
+
+	def create
+		#binding.pry
+		@project = Project.new(params[:project])
+		if @project.save
+			redirect_to projects_path	
+		else
+			redirect_to :back
+		end
+	end
+
+	def index
+		@projects=Project.all
+		@count=Project.count
+	end
+
+	def edit
+		@project = Project.find params[:id]
+	end
+
+	def update
+		@project = Project.find params[:id]
+		@project.update_attributes(params[:project])
+		redirect_to projects_path	
+		
+	end 
+
+	def destroy
+		@project = Project.find params[:id]
+		@project.destroy
+			redirect_to projects_path	
+	end
+
 end
+
